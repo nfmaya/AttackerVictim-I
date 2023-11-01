31c17bfc6b7bc40c0c8772e990edd52c3c41259e
Nestor Maya <nestormayaibarra@gmail.com>
2023-11-01 17:51:35 -0400

    Correccion de errores, hace POST pero sigue agregando mal

--- ORMExample/src/common/entities/src/main/java/com/ucab/cmcapp/common/entities/Alerta.java
+++ ORMExample/src/common/entities/src/main/java/com/ucab/cmcapp/common/entities/Alerta.java
@@ -24,7 +24,7 @@ public class Alerta implements Serializable {
     @Column(name = "TipoAlerta")
     private String _tipoAlerta;
 
-    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.PERSIST)
     @JoinColumn(name = "Victima_id")
     private Usuario _victima;
 
